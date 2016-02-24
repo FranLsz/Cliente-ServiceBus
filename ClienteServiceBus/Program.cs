b2836f0795abb9be29e7e7d71963a5c800d52c53
FranLsz <franlsz95develop@gmail.com>
2016-02-24 11:55:36 +0100

    no message

---
+++ ClienteServiceBus/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus;
+using ServiceBus;
+
+namespace ClienteServiceBus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cf = new ChannelFactory<IServicioSaludo>(new NetTcpRelayBinding(), new EndpointAddress(ServiceBusEnvironment.CreateServiceUri("sb", "Demo444", "saludo")));
+
+            cf.Endpoint.Behaviors.Add(new TransportClientEndpointBehavior()
+            {
+                TokenProvider = TokenProvider.CreateSharedSecretTokenProvider("owner", "58A9PGyNb5Qayh8zkYxAj0cDtAKKjlRcyvoyqJSr8do=")
+            });
+
+            var ch = cf.CreateChannel();
+            Console.WriteLine(ch.GetSaludo("es"));
+            cf.Close();
+
+            Console.Read();
+        }
+    }
+}
